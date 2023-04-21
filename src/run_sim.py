b8572712a2c351860cddf5b08f3bb8030e89c526
Avery Parr <averyparr@gmail.com>
2023-04-21 13:55:47 -0400

    examples of how you have to generate random numbers

--- src/run_sim.py
+++ src/run_sim.py
@@ -1,3 +1,9 @@
 import jax.numpy as jnp
+from jax import random as rand
 
 ## creating a base file to scaffold on for when we actually write things
+
+rand_key = rand.PRNGKey(678912390)
+
+print(rand.randint(rand_key,(50,),minval=0,maxval=10))
+print(rand.uniform(rand_key,(50,),minval=0,maxval=10))
