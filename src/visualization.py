920bd5bc46e79f6f6c4d9320e49800792176b200
Avery Parr <averyparr@gmail.com>
2023-04-22 16:20:28 -0400

    progress bar now works

--- src/visualization.py
+++ src/visualization.py
@@ -1,5 +1,5 @@
 from typing import Tuple
-import tqdm
+from tqdm import trange
 
 import numpy as np
 import jax.numpy as jnp
@@ -46,7 +46,7 @@ def animate_particles(r: jnp.ndarray, theta: jnp.ndarray, width: float, height: 
     ax.set_xlim(-width/2, width/2)
     ax.set_ylim(-height/2, height/2)
 
-    for frame in (pbar := tqdm(range(n_frames))):
+    for frame in (pbar := trange(n_frames)):
         pbar.set_description(f"Processing {frame}")
         
         # Clear the axis
