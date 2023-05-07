730e14b582c2804dbb19a5cf3268a7d98914633a
Erik Swanson <erik@freehawk.net>
2023-05-07 05:21:44 -0400

    more jitable

--- src/objects.py
+++ src/objects.py
@@ -59,69 +59,90 @@ class ConvexPolygon():
             edge = sorted_vertices[i] - sorted_vertices[i-1]
             normals.append([edge[1], -edge[0]] / np.linalg.norm([edge[1], -edge[0]]))
         
-        self.centroid = jnp.array(np.sum(sorted_vertices, axis=0) / sorted_vertices.shape[0])
-        self.angle = 0
+        centroid = jnp.array(np.sum(sorted_vertices, axis=0) / sorted_vertices.shape[0])
+        
+        # self.centroid = jnp.array(np.sum(sorted_vertices, axis=0) / sorted_vertices.shape[0])
+        # self.angle = 0
         self.normals_0 = jnp.array(normals)
-        self.vertices_rel = jnp.array(sorted_vertices - self.centroid)
+        self.vertices_rel = jnp.array(sorted_vertices - centroid)
         pass
 
-    def update_position(self, dr):
-        '''
-        Updates the centroid, translating by the vector dr.
-        '''
-        self.centroid += dr
-        pass
-    
-    def update_angle(self, dtheta):
-        '''
-        Updates the angle, rotating by the angle dtheta
-        '''
-        self.angle += dtheta
-        pass
-    
-    def get_vertices_normals_proj(self):
+    def get_vertices_normals_proj_jax(self, centroid: jnp.array, angle: float) -> Tuple[jnp.array, jnp.array, jnp.array]:
         '''
         Returns a tuple of arrays: (vertices, normals, projections) where vertices 
         contains the current positions of all the vertices, normals contains the current 
         set of properly oriented normals, and projections contains the projected location 
         of each edge onto its respective normal.
         '''
-        cos_theta = jnp.cos(self.angle)
-        sin_theta = jnp.sin(self.angle)
+        cos_theta = jnp.cos(angle)
+        sin_theta = jnp.sin(angle)
         rotation_matrix = jnp.array([[cos_theta, -sin_theta], [sin_theta, cos_theta]])
         
-        vertices = jnp.dot(self.vertices_rel, rotation_matrix) + self.centroid
+        vertices = jnp.dot(self.vertices_rel, rotation_matrix) + centroid
         normals = jnp.dot(self.normals_0, rotation_matrix)
         projections = jnp.einsum('ij,ij->i', vertices, normals)
         
         return vertices, normals, projections
-        
-    def get_vertices(self):
-        '''
-        Returns an array of the current location of vertices, appropriately translated
-        and rotated with respect to self.centroid and self.angle.
-        '''
-        cos_theta = jnp.cos(self.angle)
-        sin_theta = jnp.sin(self.angle)
-        rotation_matrix = jnp.array([[cos_theta, -sin_theta], [sin_theta, cos_theta]])
-        
-        vertices = jnp.dot(self.vertices_rel, rotation_matrix) + self.centroid
-        
-        return vertices
     
-    def get_normals(self):
-        '''
-        Returns an array of the current normal vectors to the polygon edges, appropriately
-        rotated by self.angle.
-        '''
+    # DO NOT USE ANYTHING BELOW THIS LINE IN JAX
+
+    # def update_position(self, dr):
+    #     '''
+    #     Updates the centroid, translating by the vector dr. DONT USE THESE IF YOU WANT TO JAX.
+    #     '''
+    #     self.centroid += dr
+    #     pass
+    
+    # def update_angle(self, dtheta):
+    #     '''
+    #     Updates the angle, rotating by the angle dtheta. DONT USE THESE IF YOU WANT TO JAX.
+    #     '''
+    #     self.angle += dtheta
+    #     pass
+    
+    # def get_vertices_normals_proj(self):
+    #     '''
+    #     Returns a tuple of arrays: (vertices, normals, projections) where vertices 
+    #     contains the current positions of all the vertices, normals contains the current 
+    #     set of properly oriented normals, and projections contains the projected location 
+    #     of each edge onto its respective normal.
+    #     '''
+    #     cos_theta = jnp.cos(self.angle)
+    #     sin_theta = jnp.sin(self.angle)
+    #     rotation_matrix = jnp.array([[cos_theta, -sin_theta], [sin_theta, cos_theta]])
         
-        cos_theta = jnp.cos(self.angle)
-        sin_theta = jnp.sin(self.angle)
-        rotation_matrix = jnp.array([[cos_theta, -sin_theta], [sin_theta, cos_theta]])
+    #     vertices = jnp.dot(self.vertices_rel, rotation_matrix) + self.centroid
+    #     normals = jnp.dot(self.normals_0, rotation_matrix)
+    #     projections = jnp.einsum('ij,ij->i', vertices, normals)
         
-        normals = jnp.dot(self.normals_0, rotation_matrix)
+    #     return vertices, normals, projections
         
-        return normals
+    # def get_vertices(self):
+    #     '''
+    #     Returns an array of the current location of vertices, appropriately translated
+    #     and rotated with respect to self.centroid and self.angle.
+    #     '''
+    #     cos_theta = jnp.cos(self.angle)
+    #     sin_theta = jnp.sin(self.angle)
+    #     rotation_matrix = jnp.array([[cos_theta, -sin_theta], [sin_theta, cos_theta]])
+        
+    #     vertices = jnp.dot(self.vertices_rel, rotation_matrix) + self.centroid
+        
+    #     return vertices
+    
+    # def get_normals(self):
+    #     '''
+    #     Returns an array of the current normal vectors to the polygon edges, appropriately
+    #     rotated by self.angle.
+    #     '''
+        
+    #     cos_theta = jnp.cos(self.angle)
+    #     sin_theta = jnp.sin(self.angle)
+    #     rotation_matrix = jnp.array([[cos_theta, -sin_theta], [sin_theta, cos_theta]])
+        
+    #     normals = jnp.dot(self.normals_0, rotation_matrix)
+        
+    #     return normals
         
    
         
