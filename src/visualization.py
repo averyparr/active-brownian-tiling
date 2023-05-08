52d7cf715c894c00bb9a48ed889a340ded1b4bc2
Avery Parr <averyparr@gmail.com>
2023-05-07 23:53:28 -0400

    update visualization to be compatible with ConvexPolygon and vertex representation of polygons

--- src/visualization.py
+++ src/visualization.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import List, Tuple
 from tqdm import tqdm
 
 import numpy as np
@@ -13,9 +13,8 @@ from constants import DEFAULT_GIF_FPS
 def animate_particles(
         r: jnp.ndarray, 
         theta: jnp.ndarray, 
-        wall_history: jnp.ndarray, 
-        width: float, 
-        height: float, 
+        poly_history: List[jnp.ndarray], 
+        box_size: float,
         show_arrows: bool=False,
         gif_filename: str="particles.gif"
         ):
@@ -29,13 +28,11 @@ def animate_particles(
         A 3D array of particle positions with shape (n_frames, n_particles, 2).
     theta : jnp.ndarray
         A 2D array of particle headings with shape (n_frames, n_particles).
-    wall_history : jnp.ndarray
-        A 4D arary of wall positions with shape (n_frames, 2, W, 2). wall_history[:,0] is 
-        start positions and wall_history[:,1] is end positions.
-    width : float
-        The width of the region to be animated.
-    height : float
-        The height of the region to be animated.
+    poly_history : jnp.ndarray
+        A (n_walls,) list of (n_frames, W_i,2) Arrays of polygon vertices, where W_i
+        is the number of vertices in polygon i. 
+    box_size : float
+        The width of the region to be animated. 
     show_arrows : bool, optional
         If True, arrows representing particle headings will be displayed in the animation. Default is False.
     gif_filename : str, optional
@@ -46,9 +43,9 @@ def animate_particles(
     None
         The function saves an animated GIF file with the specified filename.
     """
-    r = np.asarray(r)
-    theta = np.asarray(theta)
-    wall_history = [np.asarray(one_hist) for one_hist in wall_history]
+    r = np.array(r)
+    theta = np.array(theta)
+    poly_history = [np.array(single_history) for single_history in poly_history]
     
     n_frames, n_particles, _ = r.shape
 
@@ -57,16 +54,16 @@ def animate_particles(
 
     # Set up the figure and axis
     fig, ax = plt.subplots(figsize=(6, 6))
-    ax.set_xlim(-width/2, width/2)
-    ax.set_ylim(-height/2, height/2)
+    ax.set_xlim(-box_size*1.2/2, box_size*1.2/2)
+    ax.set_ylim(-box_size*1.2/2, box_size*1.2/2)
 
     for frame in (pbar := tqdm(range(n_frames))):
         pbar.set_description(f"Processing {frame}")
         
         # Clear the axis
         ax.clear()
-        ax.set_xlim(-width/2, width/2)
-        ax.set_ylim(-height/2, height/2)
+        ax.set_xlim(-box_size*1.2/2, box_size*1.2/2)
+        ax.set_ylim(-box_size*1.2/2, box_size*1.2/2)
 
         # Plot the particle positions
         positions = r[frame]
@@ -78,10 +75,10 @@ def animate_particles(
             # Plot the arrows using quiver
             ax.quiver(positions[:, 0], positions[:, 1], headings[:, 0], headings[:, 1], color='red', angles='xy', scale_units='xy', scale=1)
         
-        for wall_object in wall_history: # wall_object is a (n_frames, 2, W, 2) Array
-            wall_start_list, wall_end_list = wall_object[frame] # each a (W,2) Array
-            for w1,w2 in zip(wall_start_list,wall_end_list):
-                ax.plot([w1[0],w2[0]], [w1[1],w2[1]],c="k")#, linewidth=5)
+        for single_history in poly_history:
+            vertices = single_history[frame]
+            ax.fill(*vertices.transpose(),c="k")
+
 
         # Save the frame to the buffer
         buf = io.BytesIO()
